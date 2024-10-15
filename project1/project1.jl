3a2ac511c8348ea76574015e0ebb7d45aa8cac63
Lisa Fung <lisafung710@gmail.com>
2024-10-15 11:44:17 -0700

    create and verify graph from nodes

--- project1/project1.jl
+++ project1/project1.jl
@@ -3,6 +3,9 @@ using Printf
 using CSV
 using DataFrames
 
+using GraphPlot
+using Plots
+
 """
     write_gph(dag::DiGraph, idx2names, filename)
 
@@ -31,12 +34,32 @@ Read in data CSV file into DataFrame
 function read_data(datafile, outfile)
     # Read in CSV data
     df = DataFrame(CSV.File(datafile))
+    column_names = names(df)
+    print(typeof(column_names))
     
     # Write column names to outfile
-    column_names = names(df)
-    open(outfile, "w") do file
-        write(file, join(column_names, "\n"))
+    # open(outfile, "w") do file
+    #     write(file, join(column_names, "\n"))
+    # end
+
+    # Create graph with column names and print
+    g = DiGraph()
+    for i in eachindex(column_names)
+        add_vertex!(g)
     end
+
+    # Add arbitrary directed edges
+    add_edge!(g, 1, 2)
+    add_edge!(g, 2, 3)
+    add_edge!(g, 3, 4)
+    add_edge!(g, 4, 1)
+    add_edge!(g, 1, 3)
+    
+    gplot(g, nodelabel=column_names)
+
+    idx2names = Dict(i => column_names[i] for i in eachindex(column_names))
+    write_gph(g, idx2names, outfile)
+
     return df
 end
 
