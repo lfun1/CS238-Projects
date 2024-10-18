b925ce2b5fc830cdf71cf22cb523a0418908b677
Lisa Fung <lisafung710@gmail.com>
2024-10-17 20:26:35 -0700

    clean up code

--- project1/project1.jl
+++ project1/project1.jl
@@ -19,56 +19,71 @@ function write_gph(dag::DiGraph, idx2names, filename)
     end
 end
 
+"""
+    read_data(datafile)
+Read in data CSV file into DataFrame.
 
-function compute(infile, outfile)
+Input:
+    datafile : path to CSV data file
 
-    # WRITE YOUR CODE HERE
-    # FEEL FREE TO CHANGE ANYTHING ANYWHERE IN THE CODE
-    # THIS INCLUDES CHANGING THE FUNCTION NAMES, MAKING THE CODE MODULAR, BASICALLY ANYTHING
-
+Output:
+    Returns DataFrame of CSV data file
+"""
+function read_data(datafile)
+    return DataFrame(CSV.File(datafile))
 end
 
 """
-Read in data CSV file into DataFrame
-"""
-function read_data(datafile, outfile)
-    # Read in CSV data
-    df = DataFrame(CSV.File(datafile))
-    column_names = names(df)
-    print(typeof(column_names))
-    
-    # Write column names to outfile
-    # open(outfile, "w") do file
-    #     write(file, join(column_names, "\n"))
-    # end
+    graph_init(df)
 
-    # Create graph with column names and print
+Initializes Directed Graph (DiGraph) with specified nodes and no edges.
+
+Input: 
+    nodes : vector of nodes to create DiGraph from
+
+Output:
+    g : graph with df columns as nodes, with no edges
+    idx2names : Dict mapping node indices to column names
+"""
+function graph_init(nodes)
     g = DiGraph()
-    for i in eachindex(column_names)
+
+    # Initialize nodes
+    for i in eachindex(nodes)
         add_vertex!(g)
     end
 
-    # Add arbitrary directed edges
-    add_edge!(g, 1, 2)
-    add_edge!(g, 2, 3)
-    add_edge!(g, 3, 4)
-    add_edge!(g, 4, 1)
-    add_edge!(g, 1, 3)
-    
-    gplot(g, nodelabel=column_names)
+    # Dictionary mapping node index to column name
+    idx2names = Dict(i => nodes[i] for i in eachindex(nodes))
 
-    idx2names = Dict(i => column_names[i] for i in eachindex(column_names))
-    write_gph(g, idx2names, outfile)
-
-    return df
+    return g, idx2names
 end
 
-if length(ARGS) != 2
-    error("usage: julia project1.jl <infile>.csv <outfile>.gph")
-end
+"""
+Performs K2 search.
 
-inputfilename = ARGS[1]
-outputfilename = ARGS[2]
+Input:
+    node_order : order of all nodes
 
-# compute(inputfilename, outputfilename)
-read_data(inputfilename, outputfilename)
+"""
+
+# function K2(node_order, )
+
+
+##########################################################
+# Program Begin
+##########################################################
+
+# if length(ARGS) != 2
+#     error("usage: julia project1.jl <infile>.csv <outfile>.gph")
+# end
+
+inputfilename = "project1/data/small.csv"
+outputfilename = "project1/outputs/small_graph_v0.gph"
+
+df = read_data(inputfilename)
+nodes = names(df)
+g, idx2names = graph_init(nodes)
+# Save graph
+# write_gph(g, idx2names, outputfilename)
+
